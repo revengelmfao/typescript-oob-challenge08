89905f55b2f189385381b40d21ef3be54c6f9a44
Bradley S <bradleysantiago4@gmail.com>
2024-12-12 23:02:47 -0500

    project update

---
+++ src/classes/Wheel.ts
@@ -0,0 +1,24 @@
+// Wheel class that defines the properties of a wheel
+class Wheel {
+  // Declare properties of the Wheel class using private access modifier
+  private diameter: number;
+  private tireBrand: string;
+
+  // Constructor for the Wheel class
+  constructor(diameter: number = 18, tireBrand: string = "GoodYear") {
+    this.diameter = diameter;
+    this.tireBrand = tireBrand;
+  }
+
+  // Getter methods for the properties of the Wheel class
+  getDiameter(): number {
+    return this.diameter;
+  }
+
+  getTireBrand(): string {
+    return this.tireBrand;
+  }
+}
+
+// Export the Wheel class
+export default Wheel;
